63d17919e804f4d19278d80b6c0b2c6ab7d26298
Vedanti Sharma <vedanti28sharma@gmail.com>
2023-05-23 18:08:29 +0530

    update res-updated

--- pern/server/server.js
+++ pern/server/server.js
@@ -4,105 +4,100 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 
-
-
-
-
-
-
 app.use(express.json());
 
-//app.use(morgan("dev"));
-//***************creating middleware for server*********
-//with the help of a middle ware we can actually send a respose to the user also it
-//it should be in correct sequence
-// app.use((req,res , next) =>{
-//     console.log("yea our middleware")
-//    next();
-// }); 
-
-//GET all restaurant
-app.get("/api/v1/restaurants", async(req, res) => {
-
-    try{
-        const results = await db.query("select * from restaurants");
-
-        //**normanl response method res.send("these are the restaurants")
-        console.log(results);
-        //jason format now:-
-        res.status(200).json({
-            "status": "success",
-            results : results.rows.length,
-            data:
-            {
-                "restaurant": results.rows,
-            },
-    
-        });
-  }catch(err){
+// GET all restaurant
+app.get("/api/v1/restaurants", async (req, res) => {
+  try {
+    const results = await db.query("SELECT * FROM restaurants");
+    console.log(results);
+    res.status(200).json({
+      status: "success",
+      results: results.rows.length,
+      data: {
+        restaurant: results.rows,
+      },
+    });
+  } catch (err) {
     console.log(err);
-
-
-
-    }
- 
+  }
 });
 
-
-//GET a restaurant
-app.get("/api/v1/restaurants/:id", (req, res) => {
-    console.log(req.params);
+// GET one restaurant
+app.get("/api/v1/restaurants/:id", async (req, res) => {
+  console.log(req.params.id);
+  try {
+    const results = await db.query(
+      "SELECT * FROM restaurants WHERE id = $1",
+      [req.params.id],
+    );
 
     res.status(200).json({
-        status: "success",
-        data:{
-            restaurant : "mcdonalds"
-
-        },
+      status: "success",
+      data: {
+        restaurant: results.rows[0],
+      },
     });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-
-//CREATE a restaurant
-app.post("/api/v1/restaurants", (req, res) => {
-    console.log(req.body);
+// CREATE a restaurant
+app.post("/api/v1/restaurants", async (req, res) => {
+  console.log(req.body);
+  try {
+    const results = await db.query(
+      "INSERT INTO restaurants (name, location, price_range) VALUES ($1, $2, $3) RETURNING *",
+      [req.body.name, req.body.location, req.body.price_range]
+    );
+    console.log(results);
 
     res.status(201).json({
-        status: "success",
-        data:{
-            restaurant : "mcdonalds"
-
-        },
+      status: "success",
+      data: {
+        restaurant: results.rows[0],
+      },
     });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
+// UPDATE restaurants
+app.put("/api/v1/restaurants/:id", async (req, res) => {
+  try {
+    const results = await db.query(
+      "UPDATE restaurants SET name = $1, location = $2, price_range = $3 WHERE id = $4 RETURNING *",
+      [req.body.name, req.body.location, req.body.price_range, req.params.id]
+    );
+    console.log(results);
 
-
-//UPDATE restaurants
-app.put("/api/v1/restaurants/:id" , (req , res) =>{
- console.log(req.params.id);
- console.log(req.body);
- res.status(200).json({
-    status:"succes",
-    data:{
-        restaurant : "mcdonalds"
-    },
- });
+    res.status(200).json({
+      status: "success",
+      data: {
+        restaurant: results.rows[0],
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-//DELETE restaurant
+// DELETE restaurant
+app.delete("/api/v1/restaurants/:id", async (req, res) => {
+  try {
+    await db.query("DELETE FROM restaurants WHERE id = $1", [req.params.id]);
 
-app.delete("/api/v1/restaurants/:id " ,(req , res) =>{
     res.status(204).json({
-        status:"Succes"
+      status: "success",
     });
-
+  } catch (err) {
+    console.log(err);
+  }
 });
-
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
-    console.log(`server is started on port ${port}`)
+  console.log(`Server is started on port ${port}`);
 });
-
-
