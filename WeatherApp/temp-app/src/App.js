a443f0e2d324c8f78da8f89d690c49de830ae784
vedantiSharma <vedantis2812btechcse2024@kccitm.edu.in>
2023-07-27 15:40:27 +0530

    changes2

---
+++ WeatherApp/temp-app/src/App.js
@@ -0,0 +1,13 @@
+import Search from './components/search/search';
+import './App.css';
+
+function App() {
+  return (
+    <div classname ="Container">
+      <Search />
+    </div>
+    
+  );
+}
+
+export default App;
